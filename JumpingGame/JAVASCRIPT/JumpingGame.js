23899843faa9f98c8c502d1d885de3e6574bc6d1
Won Yong Ha <hpmsora@gmail.com>
2017-05-17 02:21:33 -0400

    Jumping Game javascript improve

---
+++ JumpingGame/JAVASCRIPT/JumpingGame.js
@@ -0,0 +1,37 @@
+var gamePiece;
+
+function startGame() {
+  gamePiece = new component(30, 30, "red", 10, 120);
+  gameArea.start();
+}
+
+var gameArea = {
+  canvas : document.createElement("canvas"),
+  start : function() {
+    this.canvas.width = $(document).width();
+    this.canvas.height = 200;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  clear : function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
+
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.update = function() {
+    ctx = gameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+function updateGameArea() {
+  gameArea.clear();
+  gamePiece.update();
+}
