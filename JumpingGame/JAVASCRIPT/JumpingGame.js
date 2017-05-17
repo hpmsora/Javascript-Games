6819ff7078b8ff8c7a2df339d22423859aca4fd2
Won Yong Ha <hpmsora@gmail.com>
2017-05-17 14:23:03 -0400

    key press add

--- JumpingGame/JAVASCRIPT/JumpingGame.js
+++ JumpingGame/JAVASCRIPT/JumpingGame.js
@@ -13,6 +13,12 @@ var gameArea = {
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.interval = setInterval(updateGameArea, 20);
+    window.addEventListener('keydown', function(e) {
+      gameArea.key = e.keyCode;
+    })
+    window.addEventListener('keyup', function(e) {
+      gameArea.key = false;
+    })
   },
   clear : function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -22,6 +28,7 @@ var gameArea = {
 function component(width, height, color, x, y) {
   this.width = width;
   this.height = height;
+  this.speedY = 0;
   this.x = x;
   this.y = y;
   this.update = function() {
@@ -29,9 +36,15 @@ function component(width, height, color, x, y) {
     ctx.fillStyle = color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
+  this.newPos = function() {
+    this.y += this.speedY;
+  }
 }
 
 function updateGameArea() {
   gameArea.clear();
+  gamePiece.speedY = 0;
+  if(gameArea.key && gameArea.key == 32) {gamePiece.speedY = -1;}
+  gamePiece.newPos();
   gamePiece.update();
 }
